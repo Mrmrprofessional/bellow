7182398f4673a680ec6913cd2419e2a00aa9f181
Mike <michael.rapp358@gmail.com>
2015-10-02 16:35:33 -0700

    add app

---
+++ app.js
@@ -0,0 +1,35 @@
+var nicksFlicks = angular.module('nicksFlicks', ['ui.router']);
+
+nicksFlicks.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    views: {
+      'header': {
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: "partials/home.html"
+      },
+    }
+  });
+
+  $stateProvider.state('reviews', {
+    url: "/reviews",
+    views: {
+      'header':{
+        templateUrl: 'partials/header.html',
+      },
+      'body': {
+        templateUrl: "partials/reviews.html",
+        controller: 'ReviewsCtrl'
+      },
+    }
+  });
+
+  $stateProvider.state('reviews.review', {
+    url: "/:reviewId",
+    templateUrl: "partials/reviews.review.html",
+    controller: 'ReviewsCtrl'
+  });
+
+});
